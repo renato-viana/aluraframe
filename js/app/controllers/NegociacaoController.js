2505080567c9948e26cafeb5c07927f4cab2c920
Renato Viana <renatoviana30@gmail.com>
2020-05-04 19:20:02 -0300

    API Reflection e as facetas de this

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -4,7 +4,10 @@ class NegociacaoController {
     this._inputData = $("#data");
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
-    this._listaNegociacoes = new ListaNegociacoes();
+    
+    this._listaNegociacoes = new ListaNegociacoes(model =>
+      this._negociacoesView.update(model));
+
     this._negociacoesView = new NegociacoesView($('#negociacoesView'));
     this._negociacoesView.update(this._listaNegociacoes);
     this._mensagem = new Mensagem();
@@ -15,12 +18,19 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
     this._listaNegociacoes.adiciona(this._criaNegociacao());
-    this._negociacoesView.update(this._listaNegociacoes);
     
     this._mensagem.texto = "Negociação adicionada com sucesso";
     this._mensagemView.update(this._mensagem);
 
     this._limpaFormulario();
+  }
+
+  apaga() {
+
+    this._listaNegociacoes.esvazia();
+
+    this._mensagem.texto = 'Negociações apagadas com sucesso';
+    this._mensagemView.update(this._mensagem);
   }
 
   _criaNegociacao() {
