935c4647d6fdda751851fa21a95cc5f1617cb3ad
Renato Viana <renatoviana30@gmail.com>
2020-05-05 16:03:10 -0300

    O padrão de projeto Proxy!

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -4,21 +4,43 @@ class NegociacaoController {
     this._inputData = $("#data");
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
-    
-    this._listaNegociacoes = new ListaNegociacoes(model =>
-      this._negociacoesView.update(model));
 
-    this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+    let self = this;
+
+    // Pense em padrão de projeto proxies como "cascas" que envolvem objetos. Dentro desse contexto, só podemos "tocar" os objetos encapsulados passando pelo proxy.
+    this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+      get(target, prop, receiver) {
+        if (
+          ["adiciona", "esvazia"].includes(prop) &&
+          typeof target[prop] == typeof(Function)
+        ) {
+          return function () {
+            console.log(`método '${prop}' interceptado`);
+
+            Reflect.apply(target[prop], target, arguments);
+
+            self._negociacoesView.update(target);
+          };
+        }
+
+        return Reflect.get(target, prop, receiver);
+      },
+    });
+
+    /* this._listaNegociacoes = new ListaNegociacoes(model =>
+      this._negociacoesView.update(model)); */
+
+    this._negociacoesView = new NegociacoesView($("#negociacoesView"));
     this._negociacoesView.update(this._listaNegociacoes);
     this._mensagem = new Mensagem();
-    this._mensagemView = new MensagemView($('#mensagemView'));
+    this._mensagemView = new MensagemView($("#mensagemView"));
     this._mensagemView.update(this._mensagem);
   }
 
   adiciona(event) {
     event.preventDefault();
     this._listaNegociacoes.adiciona(this._criaNegociacao());
-    
+
     this._mensagem.texto = "Negociação adicionada com sucesso";
     this._mensagemView.update(this._mensagem);
 
@@ -26,10 +48,9 @@ class NegociacaoController {
   }
 
   apaga() {
-
     this._listaNegociacoes.esvazia();
 
-    this._mensagem.texto = 'Negociações apagadas com sucesso';
+    this._mensagem.texto = "Negociações apagadas com sucesso";
     this._mensagemView.update(this._mensagem);
   }
 
