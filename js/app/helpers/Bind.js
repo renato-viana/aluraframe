30f2f01775c82e9b9dfc600993b57980759f9ba8
Renato Viana <renatoviana30@gmail.com>
2020-05-06 09:54:37 -0300

    Padrão de Projeto Factory

---
+++ js/app/helpers/Bind.js
@@ -0,0 +1,11 @@
+class Bind {
+    // REST operator (...)
+    constructor(model, view, ...props) {
+
+        let proxy = ProxyFactory.create(model, props, model =>  view.update(model));
+        
+        view.update(model);
+
+        return proxy;  
+    }
+}
